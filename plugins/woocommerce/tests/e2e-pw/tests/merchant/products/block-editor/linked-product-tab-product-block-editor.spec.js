38c1f1ff59eeba2d2f3d3672b06c48772e60125a
Veljko V. <veljko.vujanovic@automattic.com>
2024-11-30 08:57:00 +0100

    Add wait for category response

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/products/block-editor/linked-product-tab-product-block-editor.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/products/block-editor/linked-product-tab-product-block-editor.spec.js
@@ -86,19 +86,15 @@ test.describe( 'General tab', { tag: '@gutenberg' }, () => {
 
 				// Include in category
 				await clickOnTab( 'Organization', page );
+				const waitForCategoriesResponse = page.waitForResponse(
+					( response ) =>
+						response
+							.url()
+							.includes( '/wp-json/wp/v2/product_cat' ) &&
+						response.status() === 200
+				);
 				await page.getByLabel( 'Categories' ).click();
-				await page
-					.getByLabel( 'Categories' )
-					.type( categoryName, { delay: 100 } );
-				await page
-					.getByRole( 'option', { name: categoryName } )
-					.locator( 'label' )
-					.waitFor();
-				await expect(
-					page
-						.getByRole( 'option', { name: categoryName } )
-						.locator( 'label' )
-				).toHaveText( categoryName );
+				await waitForCategoriesResponse;
 				await page.getByLabel( categoryName ).check();
 				await page.getByLabel( `Remove Uncategorized` ).click();
 				await expect(
